214ee9ed392f5ad90571b37bffb412ca3a4890ab
TCP-23 <nstalfort@outlook.com>
2025-04-04 12:42:03 -0400

    Fixed crash when importing textures without models
    Fixed crash when importing textures without models

--- addon_source/gmf2_tools/gmf2_importer.py
+++ addon_source/gmf2_tools/gmf2_importer.py
@@ -304,10 +304,10 @@ class GM2ModelImporter(Operator):
         return combined_armature
 
     def cleanup_imported(self, context, process_obj_list, root_armature):
-        bpy.ops.object.mode_set(mode="OBJECT")
-        bpy.ops.object.select_all(action='DESELECT')
-
         if self.import_models:
+            bpy.ops.object.mode_set(mode="OBJECT")
+            bpy.ops.object.select_all(action='DESELECT')
+
             for obj in process_obj_list:
                 if obj.parent_obj is not None and obj.parent_obj in GM2ModelImporter.junk_objs:
                     bpy.data.objects[GM2ModelImporter.obj_list[obj.obj].name].select_set(True)
